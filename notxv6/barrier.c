07f657d336a7d56fa64dd8c399276ffde8199679
김현석 <kim010322@gmail.com>
2024-11-03 14:43:03 +0000

    implement barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,20 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
+
+  int n;
   
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  ++bstate.nthread;
+  n = bstate.nthread;
+  if (n < nthread) {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  } else {
+    pthread_cond_broadcast(&bstate.barrier_cond);
+    ++bstate.round;
+    bstate.nthread = 0;
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
